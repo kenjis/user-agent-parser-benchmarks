00aa06e0bf088d1b45648fd8f6a0c8bf0facac98
kenjis <kenji.uui@gmail.com>
2014-11-06 13:45:48 +0900

    Improve benchmark result output

--- bin/run-benchmarks.php
+++ bin/run-benchmarks.php
@@ -8,12 +8,13 @@ $output = __DIR__ . '/../cache/benchmark-results.json';
 $parsers = array('crossjoin-browscap', 'get_browser', 'browscap-php', 'ua-parser', 'woothee');
 
 foreach ($parsers as $parser) {
-    echo 'Benchmarking ' . $parser . ' ...' . PHP_EOL;
+    echo 'Benchmarking ' . $parser . ' ...';
     $result = file_get_contents($url . '/benchmarks/' . $parser .'.php');
     $tmp = explode("\n", $result);
     $time = (float) trim($tmp[0], ' sec');
     $memory = (int) trim($tmp[1], ' bytes');
-
+    echo "\t" . substr($time, 0, 9) . 'secs ' . "\t" . ($memory/1024)/1024 . 'MB' . PHP_EOL;
+    
     $data[$parser] = array(
         'time' => $time,
         'memory' => $memory,
