7e8481950796eb0504c8d818e6c11229c43b467f
kenjis <kenji.uui@gmail.com>
2014-11-07 16:10:55 +0900

    Change parser order

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ $config = array(
     'parseAll' => false,
     'baseUrl' => 'http://localhost:8000',
 //    'baseUrl' => 'http://localhost/user-agent-parser-benchmarks/',
-    'parsers' => array('crossjoin-browscap', 'get_browser', 'browscap-php', 'ua-parser', 'woothee'),
+    'parsers' => array('get_browser', 'browscap-php', 'crossjoin-browscap', 'ua-parser', 'woothee'),
 );
