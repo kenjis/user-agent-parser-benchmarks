42383f6eb37c3587c4c7b30a598a87635f973845
kenjis <kenji.uui@gmail.com>
2014-11-08 12:57:06 +0900

    Update get-ua-from-apache-log.sh

--- bin/get-ua-from-apache-log.sh
+++ bin/get-ua-from-apache-log.sh
@@ -4,4 +4,8 @@ cd `dirname $0`
 
 logfile="$1"
 
-cat "$logfile" | cut -f 6 -d '"' | sort | uniq > ua-list.txt
+grep -v "/assets/" "$logfile" | grep -v "/favicon.ico" | grep -v "/robots.txt" \
+ | cut -f 6 -d '"' | grep -v '^-' | head -100
+exit
+
+cat "$logfile" | cut -f 6 -d '"' | sort | uniq | sort > ua-list.txt
