3b6419fd41fd7541cda482a4c9a4cb62251ec322
kenjis <kenji.uui@gmail.com>
2014-11-08 13:29:12 +0900

    Change how to benchmark, and update the result
    Environment:
      CentOS 6.5 (64bit) on VirtualBox (Host: MBA OS X Yosemite)
      PHP 5.5.18
        Zend OPcache v7.0.4-dev
        Xdebug v2.2.5
      Apache 2.2.15

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 // global config
 $config = array(
-    'parseAll' => false,
+    'parseAll' => true,
     'baseUrl' => 'http://localhost:8000',
 //    'baseUrl' => 'http://localhost/user-agent-parser-benchmarks/',
     'parsers' => array('get_browser', 'browscap-php', 'crossjoin-browscap', 'ua-parser', 'woothee'),
