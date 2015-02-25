f71615c9af6fab20861ee143e11615607bab61ad
kenjis <kenji.uui@gmail.com>
2015-02-25 11:16:39 +0900

    Add message output

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -4,8 +4,11 @@ cd `dirname $0`
 
 chmod o+w ../cache/*.txt
 
+echo "Updating ua-parser data..."
 php ../vendor/bin/uaparser.php ua-parser:update
 
+echo "Updating browscap-php data..."
 php update-browscap-php.php
 
+echo "Updating crossjoin-browscap data..."
 php update-crossjoin-browscap.php
