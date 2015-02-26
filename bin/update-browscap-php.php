2c9623c04df8f4025c9a7395a35c3b3517971399
kenjis <kenji.uui@gmail.com>
2015-02-26 09:21:52 +0900

    Add time/memory output to update browscap.ini file

--- bin/update-browscap-php.php
+++ bin/update-browscap-php.php
@@ -5,9 +5,15 @@ ini_set('memory_limit', -1);
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config.php';
 
-$cacheDir = $config['cacheDir'];
-
 use phpbrowscap\Browscap;
 
+$bench = new Ubench;
+$bench->start();
+
+$cacheDir = $config['cacheDir'];
 $browscap = new Browscap($cacheDir);
 $browscap->updateCache();
+
+$bench->end();
+echo ' ', $bench->getTime(true), ' secs ', PHP_EOL;
+echo ' ', number_format($bench->getMemoryPeak(true)), ' bytes', PHP_EOL;
--- bin/update-crossjoin-browscap.php
+++ bin/update-crossjoin-browscap.php
@@ -5,7 +5,14 @@ ini_set('memory_limit', -1);
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config.php';
 
+$bench = new Ubench;
+$bench->start();
+
 $cacheDir = $config['cacheDir'];
 \Crossjoin\Browscap\Cache\File::setCacheDirectory($cacheDir);
 $browscap = new \Crossjoin\Browscap\Browscap();
 $settings = $browscap->getBrowser()->getData();
+
+$bench->end();
+echo ' ', $bench->getTime(true), ' secs ', PHP_EOL;
+echo ' ', number_format($bench->getMemoryPeak(true)), ' bytes', PHP_EOL;
